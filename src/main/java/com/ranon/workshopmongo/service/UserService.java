45c0048dc498dba831b43e2e07aaa2be5175e779
Ranon Emerick <ranonemerick@gmail.com>
2023-05-08 15:28:37 -0300

    Obtendo usuario por ID

--- src/main/java/com/ranon/workshopmongo/service/UserService.java
+++ src/main/java/com/ranon/workshopmongo/service/UserService.java
@@ -1,21 +1,28 @@
 package com.ranon.workshopmongo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ranon.workshopmongo.domain.User;
 import com.ranon.workshopmongo.repository.UserRepository;
+import com.ranon.workshopmongo.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repository;
-	
-	public List<User>finAll(){
+
+	public List<User> finAll() {
 		return repository.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
 	}
 
 }
